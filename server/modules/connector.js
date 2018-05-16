94f9c698a7dce82ebd55f9fa25ff07250568a989
Alexander Ivanov <aivanov@informatica.ru>
2018-05-16 15:37:43 +0300

    express-redis-session

--- server/modules/connector.js
+++ server/modules/connector.js
@@ -1,6 +1,7 @@
 export { default as logger } from 'tools/logger/config';
-export {
-  default as reqSession,
-} from 'tools/express-session-sequelize/config';
+// export {
+//   default as reqSession,
+// } from 'tools/express-session-sequelize/config';
+export { default as redis } from 'tools/redis/config';
 
 export { default as sessionInfo } from './sessionInfo';
