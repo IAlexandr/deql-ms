26d208728a8423219ee404d5935cd576601ac8b6
Alexander Ivanov <aivanov@informatica.ru>
2018-05-18 14:55:52 +0300

    redis client, get prisma conOps

--- server/utils/dependencies.js
+++ server/utils/dependencies.js
@@ -1,5 +1,6 @@
 // import session from 'tools/express-session-sequelize';
 import session from 'tools/redis/express-session';
+import { getValue, connect as redisConnect } from 'tools/redis';
 import logger from 'tools/logger';
 //Выбираем базу данных
 import { init } from 'tools/db/sequelize';
@@ -9,6 +10,8 @@ const { debug, time } = logger('project.dependencies');
 
 export const dependencies = async function({ app }) {
   const initialized = time('initializing');
+  await redisConnect();
+
   const db = await init({
     dbConfig: options.config.sequelize,
     NODE_ENV: options.config.NODE_ENV,
